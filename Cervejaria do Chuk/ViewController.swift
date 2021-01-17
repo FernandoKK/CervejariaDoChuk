9c5a2c1f7942743253ff07b9bbd7dad1c0b6e11a
Fernando Kovaltchuk <fernando.kovaltchuk@gft.com>
2021-01-17 19:23:35 -0300

    Initial Commit

---
+++ Cervejaria do Chuk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cervejaria do Chuk
+//
+//  Created by Kovaltchuk, Fernando Cesar on 17/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
